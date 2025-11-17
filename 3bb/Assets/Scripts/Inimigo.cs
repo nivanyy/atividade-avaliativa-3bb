a943b8d5aac37721659e4b5d9211497842e073e6
nivanyy <silva.nivany@escolar.ifrn.edu.br>
2025-11-17 11:22:03 -0300

    17/11

--- 3bb/Assets/Scripts/Inimigo.cs
+++ 3bb/Assets/Scripts/Inimigo.cs
@@ -2,10 +2,41 @@ using UnityEngine;
 
 public class Inimigo : Personagem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
+    
+    [SerializeField] private int dano = 1;
+
+    public float raioDeVisao = 1;
+    public CircleCollider2D visaoCollider;
+    
+    [SerializeField] private Transform posicaoPlayer;
+    
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    
+    
+    private bool andando = false;
+
+    public void setDano(int dano)
+    {
+        this.dano = dano;
+    }
+
+
+    public int getDano()
+    {
+        return this.dano;
+    }
+    
+    
+    
+    
+    
+    
     void Start()
     {
-        
+    spriteRenderer = GetComponent<SpriteRenderer>();    
     }
 
     // Update is called once per frame
