f461dbe80b7b4a8712f73b1c06ef939b0ab74261
nivanyy <silva.nivany@escolar.ifrn.edu.br>
2025-11-18 11:57:50 -0300

    18/11

--- 3bb/Assets/Scripts/Inimigo.cs
+++ 3bb/Assets/Scripts/Inimigo.cs
@@ -2,46 +2,117 @@ using UnityEngine;
 
 public class Inimigo : Personagem
 {
-    
-    
-    
     [SerializeField] private int dano = 1;
-
-    public float raioDeVisao = 1;
-    public CircleCollider2D visaoCollider;
     
-    [SerializeField] private Transform posicaoPlayer;
+    public float raioDeVisao = 1;
+    public CircleCollider2D _visaoCollider2D;
+
+    [SerializeField] private Transform posicaoDoPlayer;
     
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-    
-    
-    private bool andando = false;
 
+    private bool andando = false;
+    
     public void setDano(int dano)
     {
         this.dano = dano;
     }
-
-
     public int getDano()
     {
         return this.dano;
     }
     
-    
-    
-    
-    
+                
     
     void Start()
     {
-    spriteRenderer = GetComponent<SpriteRenderer>();    
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        
+        if (posicaoDoPlayer == null)
+        {
+            posicaoDoPlayer =  GameObject.Find("Player").transform;
+           // posicaoDoPlayer =  GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        }
+        
+        raioDeVisao = _visaoCollider2D.radius;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        andando = false;
+
+        if (getVida() > 0)
+        {
+
+            if (posicaoDoPlayer.position.x - transform.position.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+
+            if (posicaoDoPlayer.position.x - transform.position.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+
+
+            if (posicaoDoPlayer != null &&
+                Vector3.Distance(posicaoDoPlayer.position, transform.position) <= raioDeVisao)
+            {
+                Debug.Log("Posição do Pluer" + posicaoDoPlayer.position);
+
+                transform.position = Vector3.MoveTowards(transform.position,
+                    posicaoDoPlayer.transform.position,
+                    getVelocidade() * Time.deltaTime);
+
+                andando = true;
+            }
+
+        }
         
+        if (getVida() <= 0)
+        {
+            animator.SetTrigger( name:"Morto");
+        }
+
+        animator.SetBool(name:"Andando", andando);
     }
+    
+   private void onCollisionEnter2D(Collision2D collision)
+   {
+       if (collision.gameObject.CompareTag("Player") && getVida()>0)     
+       {
+        //causa dano no player
+        int novaVida =collision.gameObject.GetComponent<Personagem>().getVida();
+        
+       }
+    
+    
+    }
+
+    public void desativa()
+    {
+        //desativa o objeto do Inimigo
+        //gameObject.SetActive(false);
+        Destroy(gameObject);
+        Debug.Log("Teste...");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && getVida() > 0)
+        { 
+            // Causa dano ao Player
+            int novaVida = collision.gameObject.GetComponent<Personagem>().getVida() - getDano();
+            collision.gameObject.GetComponent<Personagem>(). setVida(novaVida);
+
+            //collision.gameObject.GetComponent<Personagem>().recebeDano(getDano());
+            
+            //sera a vida do inimigo
+          
+        }
+    }
+
 }
