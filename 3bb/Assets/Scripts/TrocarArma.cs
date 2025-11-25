7795a48959fc68c09cd080dd3a907cbf3454f15d
nivanyy <silva.nivany@escolar.ifrn.edu.br>
2025-11-24 23:16:46 -0300

    244//111

---
+++ 3bb/Assets/Scripts/TrocarArma.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TrocarArma : MonoBehaviour
+{
+    public GameObject arma1;
+    public GameObject arma2;
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            arma1.SetActive(true);
+            arma2.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            arma1.SetActive(false);
+            arma2.SetActive(true);
+        }
+        
+        
+        
+        
+    }
+}
